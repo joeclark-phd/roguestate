440e0468dbf4caa443e124de214a308c3769539c
Joseph Clark <joeclark77@hotmail.com>
2014-04-01 17:18:38 -0700

    bug with sprites

---
+++ program/sprite_test.py
@@ -0,0 +1,14 @@
+import pyglet
+
+window = pyglet.window.Window()
+robot = pyglet.image.load('robot.jpg')
+sprite = pyglet.sprite.Sprite(robot,x=200,y=200)
+
+
+@window.event
+def on_draw():
+    window.clear()
+    robot.blit(100,100)
+    sprite.draw()
+
+pyglet.app.run()
