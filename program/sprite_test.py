70392cc528bf17a6119409b43c6c02c435a208e4
Joseph Clark <joeclark77@hotmail.com>
2014-04-02 12:18:00 -0700

    changed comments in sprite_test.py

--- program/sprite_test.py
+++ program/sprite_test.py
@@ -1,19 +1,20 @@
-# This file is here to demonstrate a bug: image.blit() is working, but sprite.draw() seems to be broken.
-# I have used sprite.draw() before, and this code worked on my other computer.  The differences are an
-# upgrade from Python 3.3 to 3.4, and possibly some changes to pyglet(???).  Otherwise I'm stumped.
+# This file WAS here to demonstrate a bug: image.blit() is working, but sprite.draw() seems to be broken.
+# I have used sprite.draw() before, and this code worked on my other computer.
+# The difference, as it turned out, was my AMD video card.  A tweak to pyglet/sprite.py fixed it.
+# I'm leaving this code here for now, to make sure I haven't broken the game for others!
 
 
 import pyglet
 
 window = pyglet.window.Window()
 robot = pyglet.image.load('robot.jpg')
-sprite = pyglet.sprite.Sprite(robot,x=200,y=200)
+sprite = pyglet.sprite.Sprite(robot,x=400,y=200)
 
 
 @window.event
 def on_draw():
     window.clear()
-    #robot.blit(100,100)
-    sprite.draw()
+    robot.blit(100,100)  # you should see one robot on the left
+    sprite.draw()  # and another robot on the right
 
 pyglet.app.run()
