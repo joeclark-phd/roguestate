856269663e77ecf577a356e2fc8afc8185b482df
Joseph Clark <joeclark77@hotmail.com>
2014-04-02 12:33:26 -0700

    changed comments in sprite_test.py

--- roguestate.py
+++ roguestate.py
@@ -5,11 +5,14 @@
 # here I simply add the containing folder to the PYTHONPATH
 # for the duration of execution. This does not need to be
 # repeated in every script.
+
 import sys
-cwd = sys.path[0] # current working directory
+from os import path
+
+cwd = sys.path[0]  # current working directory
 
 # i can have as many of these as necessary...
-sys.path.insert(0,cwd + "\program")
+sys.path.insert(0,path.join(cwd,"program"))
 
 
 # this launches the game window
